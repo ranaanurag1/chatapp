670c40918d88c37f46ea0cb30ddb2b765e310057
ranaanurag1 <126869676+ranaanurag1@users.noreply.github.com>
2023-03-03 16:01:38 +0530

    chatApp_intermediate

---
+++ index.js
@@ -0,0 +1,18 @@
+//this is node server//
+
+const io = require('socket.io')(8000)
+
+const users = {};
+
+io.on('connection', socket =>{
+    socket.on('new-user-joined', name =>{
+        console.log("new user", name)
+        users[socket.id] = name;
+        socket.broadcast.emit('user-joined', name);
+
+    });
+
+    socket.on('send', message =>{
+        socket.broadcast.emit('receive' , {message: message, name: user[socket.id]});
+    }); 
+});
